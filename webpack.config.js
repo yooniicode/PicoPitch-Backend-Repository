4e053b8892ff91530a48d9159fa59648c6f255ef
chadoli27 <jasminekim1009@gmail.com>
2024-07-23 21:46:55 +0900

    webpack mode 추가

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,9 @@ import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+
 export default {
+    mode: 'development',
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
