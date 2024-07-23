9ffe718e38e5cb06bace9f6cbfdf071b814c8120
chadoli27 <jasminekim1009@gmail.com>
2024-07-24 00:35:32 +0900

    npm run build 작동하도록 webpack 수정

--- config/response.js
+++ config/response.js
@@ -2,9 +2,9 @@
 
 export const response = ({ isSuccess, code, message }, result) => {
     return {
-        isSuccess: isSuccess,
-        code: code,
-        message: message,
-        result: result
-    }
+        isSuccess,
+        code,
+        message,
+        result
+    };
 };
