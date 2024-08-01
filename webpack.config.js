c3470d54879ccb763648fba99ab06ba4b22549ab
parkhs21 <2001phs92@gmail.com>
2024-08-01 15:42:46 +0900

    fix: 기존으로 복귀

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,46 @@
-import nodeExternals from "webpack-node-externals";
-import path from "path";
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
-export default {
-    mode: "development",
+
+module.exports = {
+
+    mode: 'development',
+    context: path.resolve(__dirname),
     entry: {
         app: './index.js',
     },
     output: {
-        path: path.resolve('./dist'),
-        filename: "main.cjs",
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+        libraryTarget: 'commonjs2'
     },
     module: {
         rules: [
             {
                 test: /\.js$/,
                 use: {
-                    loader: "babel-loader",
+                    loader: 'babel-loader',
                     options: {
-                        presets: ["@babel/preset-env"],
+                        presets: ['@babel/preset-env'],
+                        plugins: ['@babel/plugin-transform-modules-commonjs'] // 여기 추가
                     },
                 },
                 exclude: /node_modules/,
             },
         ],
     },
-    target: "node",
+    resolve: {
+        fallback: {
+            "http": require.resolve("http-browserify"),
+            "util": require.resolve("util/"),
+            "path": require.resolve("path-browserify"),
+        },
+    },
+    plugins: [
+        new NodePolyfillPlugin()
+    ],
+    target: 'node',
     externalsPresets: {
         node: true,
     },
