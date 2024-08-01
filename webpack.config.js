e0523a6869e7977f268af55542dc30e177d7f6f4
parkhs21 <2001phs92@gmail.com>
2024-08-01 16:03:41 +0900

    chore: cicd 파일 재설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,16 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
-const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
 
 module.exports = {
-
     mode: 'development',
-    context: path.resolve(__dirname),
+    context: __dirname,
     entry: {
         app: './index.js',
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
-        libraryTarget: 'commonjs2'
+        filename: 'main.js'
     },
     module: {
         rules: [
@@ -23,23 +20,12 @@ module.exports = {
                     loader: 'babel-loader',
                     options: {
                         presets: ['@babel/preset-env'],
-                        plugins: ['@babel/plugin-transform-modules-commonjs'] // 여기 추가
                     },
                 },
                 exclude: /node_modules/,
             },
         ],
     },
-    resolve: {
-        fallback: {
-            "http": require.resolve("http-browserify"),
-            "util": require.resolve("util/"),
-            "path": require.resolve("path-browserify"),
-        },
-    },
-    plugins: [
-        new NodePolyfillPlugin()
-    ],
     target: 'node',
     externalsPresets: {
         node: true,
