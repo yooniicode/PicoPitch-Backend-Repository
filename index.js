fc60b7e774ebc208c883f8ed8cf41e2356a23f96
Yoonji Lee <estelle0329@ewha.ac.kr>
2024-08-11 16:24:55 +0900

    chore: Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import express from 'express';
 import SwaggerUi from 'swagger-ui-express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import Sequelize from 'sequelize';
 
 import { specs } from './config/swagger.config.js';
 import { response } from './config/response.js';
@@ -15,17 +14,6 @@ import { scriptRoute } from './src/script/script.route'
 dotenv.config();    // .env 파일 사용 (환경 변수 관리)
 
 const app = express();
-const sequelize = new Sequelize('postgres://username:password@localhost:5432/database_name');
-
-// Test the database connection
-sequelize.authenticate()
-    .then(() => {
-        console.log('Database connection has been established successfully.');
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    });
-
 
 // server setting - veiw, static, body-parser etc..
 app.set('port', process.env.PORT || 8000)   // 서버 포트 지정
